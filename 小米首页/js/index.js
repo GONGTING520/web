69c96104254687e90db862b39b3f0c2f1a603eba
GONGTING520 <954160311@qq.com>
2017-10-22 18:26:02 +0800

    将明星产品和为你推荐部分统一的地方封装成bindingEvent函数 统一做特效

--- 小米首页/js/index.js
+++ 小米首页/js/index.js
@@ -1,4 +1,4 @@
-//轮播图交互
+//最上方轮播图交互开始
 var oSlideshowIndexImg = document.getElementById('slideshow-index-img');
 var aImgLi = oSlideshowIndexImg.getElementsByTagName('li');//获取轮播图图片
 var oSlideshowIndexArrow = document.getElementById('slideshow-index-arrow');//获取箭头的div
@@ -68,59 +68,77 @@ function changeImg(index) {
     aImgIndex[index].className = 'img-item selected';
     aImgLi[index].className = 'slideshow-li slideshow-selected';
 }
+//最上方轮播图交互结束
 
 
 
-
-//明星产品的左右切换
-var oStarItemDiv = document.getElementById('star-item');//获取明星产品的大div
-var oStarItemImgDiv = document.getElementById('star-item-img');//获取装内容的ul的div
-var aStarDiv = oStarItemDiv.getElementsByTagName('div');//获取明星产品下面的所有div
-var oTitleArrowDiv = null;//定义明星产品下面的箭头div
-//获取明星产品下面的箭头div
-for(i = 0; i < aStarDiv.length ; i++){
-    if(aStarDiv[i].className == 'title-arrow'){
-        oTitleArrowDiv = aStarDiv[i];
-    }
-}
-var oStarPrev = oTitleArrowDiv.getElementsByTagName('a')[0];//获取明星产品下面的向左箭头
-var oStarNext = oTitleArrowDiv.getElementsByTagName('a')[1];//获取明星产品下面的向右箭头
-var iStarFlag = 2;//定义一个标识位用来记录箭头目前的状态，1表示左箭头可以按下，2表示右箭头可以按下
-var oStarItemImgUl = oStarItemImgDiv.getElementsByTagName('ul')[0];//获取装内容的ul
-//定义左右切换的定时器
-var StarTimer;
-function setStarTimer() {
-    StarTimer = setInterval(function () {
-        if(iStarFlag == 1){
-            oStarPrev.onclick();
-        }else {
-            oStarNext.onclick()
+//明星产品的左右切换 和 为你推荐的左右切换开始
+//明星产品元素的获取开始
+    var oStarItemDiv = document.getElementById('star-item');//获取明星产品的大div
+    var oStarItemImgDiv = document.getElementById('star-item-img');//获取装内容的ul的div
+    var aStarDiv = oStarItemDiv.getElementsByTagName('div');//获取明星产品下面的所有div
+    bindingEvent(aStarDiv,oStarItemImgDiv);//调用绑定事件函数
+//明星产品元素的获取结束
+//为你推荐元素的获取开始
+    var oContainerRecommend = document.getElementById('container-recommend');//获取为你推荐的大div
+    var oRecommendDiv = document.getElementById('recommend');//获取装内容的ul的div
+    var aRecommendDiv = oContainerRecommend.getElementsByTagName('div');//获取为你推荐下面的所有div
+    bindingEvent(aRecommendDiv,oRecommendDiv);//调用绑定事件函数
+//为你推荐元素的获取结束
+//明星产品的左右切换 和 为你推荐的左右切换结束
+/**
+ * 封装绑定事件的函数
+ * 需要传两个参数divArr 和 ul
+ * divArr 传进来所有的div，用来寻找装箭头的div，并给div下面的两个箭头添加点击事件
+ * oDiv 传进来的是装ul的div，用来寻找要被移动的ul对象
+ * */
+function bindingEvent(divArr,oDiv) {
+    var oTitleArrowDiv = null;//定义divArr下面的箭头div
+    for(i = 0; i < divArr.length ; i++){
+        if(divArr[i].className == 'title-arrow') {
+            oTitleArrowDiv = divArr[i];//获取divArr下面的箭头div
         }
-    },5000)
-}
+    }
+    var oPrev = oTitleArrowDiv.getElementsByTagName('a')[0];//获取divArr下面的向左箭头
+    var oNext = oTitleArrowDiv.getElementsByTagName('a')[1];//获取divArr下面的向右箭头
+    oNext.StarFlag = true;//定义一个标识位用来记录箭头目前的状态，true表示箭头可以按下，false表示箭头可以按下
+    oPrev.StarFlag = false;
+    var oUl = oDiv.getElementsByTagName('ul')[0];//获取装内容的ul
+    var Timer;//定义左右切换的定时器
+//封装定时器的方法
+    function setTimer() {
+        Timer = setInterval(function () {
+            if(oPrev.StarFlag){
+                oPrev.onclick();
+            }else {
+                oNext.onclick();
+            }
+        },5000)
+    }
 //调用左右切换的定时器
-setStarTimer();
+    setTimer();
 //添加向右箭头的点击事件
-oStarNext.onclick = function(){
-    if(iStarFlag == 2){
-        clearInterval(StarTimer);
-        setStarTimer();
-        oStarItemImgUl.style.left = -1226 + 'px';
-        iStarFlag = 1;
-        this.className = 'title-next';
-        oStarPrev.className = 'title-prev selected';
-    }
-};
+    oNext.onclick = function(){
+        if(oNext.StarFlag){
+            clearInterval(Timer);
+            setTimer();
+            oUl.style.left = -1226 + 'px';
+            oNext.StarFlag = false;
+            oPrev.StarFlag = true;
+            oNext.className = 'title-next';
+            oPrev.className = 'title-prev selected';
+        }
+    };
 //添加向左箭头的点击事件
-oStarPrev.onclick = function(){
-    if(iStarFlag == 1){
-        clearInterval(StarTimer);
-        setStarTimer();
-        oStarItemImgUl.style.left = 0;
-        iStarFlag = 2;
-        this.className = 'title-prev';
-        oStarNext.className = 'title-next selected';
-    }
-};
-
-
+    oPrev.onclick = function(){
+        if(oPrev.StarFlag){
+            clearInterval(Timer);
+            setTimer();
+            oUl.style.left = 0;
+            oNext.StarFlag = true;
+            oPrev.StarFlag = false;
+            oPrev.className = 'title-prev';
+            oNext.className = 'title-next selected';
+        }
+    };
+}
