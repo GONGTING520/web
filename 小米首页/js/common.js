3ba7c5ec028165dcd8d5d8c972baf2e24604bd61
GONGTING520 <954160311@qq.com>
2017-10-22 16:19:57 +0800

    更新搜索框：让滑入的时候添加一个class，样式为边框颜色的变化

--- 小米首页/js/common.js
+++ 小米首页/js/common.js
@@ -1,6 +1,7 @@
 //搜索框交互
 var oIndex = document.getElementById('index');
 var oSearch = oIndex.getElementsByTagName('input')[0];//获取搜索框
+var oSubmit = oIndex.getElementsByTagName('button')[0];//获取搜索按钮
 var aIndexUl = oIndex.getElementsByTagName('ul');//获取index下所有的ul
 var oSearchList = null;//定义搜索列表
 for(var i = 0 ; i < aIndexUl.length ; i++){
@@ -13,6 +14,8 @@ var aHotWords = oSearchHotWords.getElementsByTagName('a');
 // 给输入框添加获取焦点事件
 oSearch.onfocus = function () {
     oSearchList.style.display = 'block';
+    oSubmit.className = 'submit click';
+    oSearch.className = 'search click';
     for(var i = 0 ; i < aHotWords.length ; i++){
         aHotWords[i].style.opacity = 0;
     }
@@ -20,6 +23,8 @@ oSearch.onfocus = function () {
 // 给输入框添加失去焦点事件
 oSearch.onblur = function(){
     oSearchList.style.display = 'none';
+    oSubmit.className = 'submit';
+    oSearch.className = 'search';
     for(var i = 0 ; i < aHotWords.length ; i++){
         aHotWords[i].style.opacity = 1;
     }
