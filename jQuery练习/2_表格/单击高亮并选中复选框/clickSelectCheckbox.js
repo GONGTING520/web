1024ed80c107563a6aeacabe949317625524c15e
GONGTING520 <954160311@qq.com>
2017-11-16 19:53:10 +0800

    优化单击选中复选框的js文件

--- jQuery练习/2_表格/单击高亮并选中复选框/clickSelectCheckbox.js
+++ jQuery练习/2_表格/单击高亮并选中复选框/clickSelectCheckbox.js
@@ -2,12 +2,16 @@ $(function () {
     $('table tbody tr:even').addClass('yellow');
     $('table tbody tr:odd').addClass('orange');
     $('table tbody tr').on('click', function () {
-        var $Chioce = $(this).find('input[name="chioce"]');
-        if ($Chioce.prop('checked')) {
-            $(this).removeClass('selected');
-        } else {
-            $(this).addClass('selected');
-        }
-        $Chioce.get(0).checked = !$Chioce.get(0).checked;
+        // var $Chioce = $(this).find('input[name="chioce"]');
+        // if ($Chioce.prop('checked')) {
+        //     $(this).removeClass('selected');
+        // } else {
+        //     $(this).addClass('selected');
+        // }
+        // $Chioce.get(0).checked = !$Chioce.get(0).checked;
+        $(this).toggleClass('selected').find('input[name="chioce"]').prop('checked', function (index, oldValue) {
+            return !oldValue;
+        });
     });
+    $('table tbody tr:eq(2)').trigger('click');
 });
