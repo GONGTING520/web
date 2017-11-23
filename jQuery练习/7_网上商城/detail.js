1b7fe17f9c0140c3a9e884660c983d14ed32636c
GONGTING520 <954160311@qq.com>
2017-11-23 16:30:07 +0800

    网上商城物品展览页的js文件，包括评分部分的功能

---
+++ jQuery练习/7_网上商城/detail.js
@@ -0,0 +1,50 @@
+$(function () {
+    // 放大镜中的选项卡部分开始
+    // 放大镜中的选项卡部分结束
+
+
+
+
+    // 评分部分开始
+    var $scoreLi = $('#goods-selected .score li');
+    //定义一个数组用来装滑入时更改class之前每个li的class
+    var aOldClaName = [];
+    $scoreLi.each(function () {
+        aOldClaName.push(this.className);
+    });
+    $scoreLi.hover(function () {
+        // 获得此li原来的class,在原来class的基础上加上‘-hover’
+        var claName = this.className.split(' ')[0] + '-hover';
+        // 给所有li更改class
+        changeClass(claName);
+    }, function () {
+        // 将所有的class都重置回划之前的class
+        $scoreLi.each(function (index) {
+            this.className = aOldClaName[index];
+        });
+    }).on('click', function () {
+        aOldClaName = [];
+        // 先获取li原来的class，再加上‘-selected’
+        var selectName = this.className.split(' ')[0] + '-selected';
+        changeClass(selectName);
+        // 将选择结果记录在aOldClaName数组中
+        $scoreLi.each(function () {
+            aOldClaName.push(this.className);
+        });
+        alert('您给此商品的评分是：' + ($(this).index() + 1) + '分');
+    });
+
+    /**
+     * 封装改变class函数,第一个class不变，只更改第二个class
+     * 输入claName 表示要更改的类名
+     * */
+    function changeClass(claName) {
+        // 循环所有li
+        $scoreLi.each(function () {
+            // 获取当前li的class数组，第一个为原来的class，不能改
+            var aThisClaName = this.className.split(' ');
+            this.className = aThisClaName[0] + ' ' + claName;
+        });
+    }
+    // 评分部分结束
+});
