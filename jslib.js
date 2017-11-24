8407b6fe6055fbc65c938c0f2ca23052e5b89131
GONGTING520 <954160311@qq.com>
2017-11-24 22:57:18 +0800

    增加函数可以实现对象的合并

--- jslib.js
+++ jslib.js
@@ -2,7 +2,7 @@
  * @Author: mikey.gongting 
  * @Date: 2017-11-05 17:01:01 
  * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2017-11-11 22:01:21
+ * @Last Modified time: 2017-11-24 22:56:51
  */
 
 
@@ -11,10 +11,10 @@ function addEvent(elem, type, handler) {
     //能力检测，判断elem有无addEventListener方法，标准浏览器
     if (elem.addEventListener) {
         elem.addEventListener(type, handler);
-    } else if (elem.attachEvent) {//IE浏览器
+    } else if (elem.attachEvent) { //IE浏览器
         elem.evt = function () {
             //因为点击时立即执行，所以用call或者apply
-            handler.call(elem);//将此方法的this指向elem，不设置this指向window
+            handler.call(elem); //将此方法的this指向elem，不设置this指向window
         };
         elem.attachEvent('on' + type, elem.evt);
     } else {
@@ -27,7 +27,7 @@ function removeEvent(elem, type, handler) {
     //能力检测，判断elem有无removeEventListener方法，标准浏览器
     if (elem.removeEventListener) {
         elem.removeEventListener(type, handler);
-    } else if (elem.detachEvent) {//IE浏览器
+    } else if (elem.detachEvent) { //IE浏览器
         elem.detachEvent('on' + type, elem.evt);
     } else {
         elem['on' + type] = null;
@@ -35,13 +35,13 @@ function removeEvent(elem, type, handler) {
 }
 
 //封装函数实现深拷贝，传进一个参数表示要克隆的原对象,返回克隆好的新对象
-function clone(obj){
+function clone(obj) {
     //定义一个新的对象
-    var newObj ={};
-    for(var p in obj){
-        if(typeof(obj[p]) === 'object'){
+    var newObj = {};
+    for (var p in obj) {
+        if (typeof (obj[p]) === 'object') {
             newObj[p] = clone(obj[p]);
-        }else{
+        } else {
             newObj[p] = obj[p];
         }
     }
@@ -51,11 +51,11 @@ function clone(obj){
 //封装函数通过className获取元素,传入类名和所属对象,所属对象不写则为document
 function getByClass(clsName, context) {
     context = context || document;
-    if (context.getElementsByClassName) {//能力检测,看是否有此函数
+    if (context.getElementsByClassName) { //能力检测,看是否有此函数
         return context.getElementsByClassName('clsName');
     }
-    var result = [];//定义数组来装最终的结果集
-    var aList = context.getElementsByTagName('*');//获取context下所有的标签
+    var result = []; //定义数组来装最终的结果集
+    var aList = context.getElementsByTagName('*'); //获取context下所有的标签
     for (var i = 0; i < aList.length; i++) {
         if (aList[i].className == clsName) {
             result.push(aList[i]);
@@ -70,25 +70,47 @@ function getById(idname) {
 
 //封装函数获取elem的第一个孩子元素节点
 function getFirstChild(elem) {
-    elem = elem && elem.firstChild;//先判断elem是否为空
+    elem = elem && elem.firstChild; //先判断elem是否为空
     return elem.nodeType == 1 ? elem : next(elem);
 }
 
 //封装函数获取elem的下一个兄弟元素节点
 function next(elem) {
     do {
-        elem = elem && elem.nextSibling;//先判断elem是否为空
+        elem = elem && elem.nextSibling; //先判断elem是否为空
     } while (elem && elem.nodeType != 1);
     //先判断elem是否为null，在判断nodeType
     return elem;
 }
 
-/*封装函数返回一个html集，里面装的是标签名为tagName的元素
-* 传进一个元素表示在那个元素中找标签
-* 传进标签名，表示要找的标签
-*/
+/**
+ * 封装函数返回一个html集，里面装的是标签名为tagName的元素
+ * 传进一个元素表示在那个元素中找标签
+ * 传进标签名，表示要找的标签
+ */
 function getByTag(elem, tagName) {
     //若传进了elem则为它本身，若他为null则elem为document
     elem = elem ? elem : document;
     return elem.getElementsByTagName(tagName);
+}
+
+/**
+ * 封装函数，实现对象的合并
+ * 传入要合并到的对象target，和被合并的对象数组arr
+ */
+function extend(target, arr) {
+    // 循环对象数组
+    for (var i = 0; i < arr.length; i++) {
+        // 循环对象中的每一个属性
+        for (var p in arr[i]) {
+            // 若下标为i的元素为一个对象，则应调用自己进行合并
+            if (typeof (arr[i][p]) == 'object') {
+                var arr1 = [];
+                arr1.push(arr[i][p]);
+                arguments.callee(target[p], arr1);
+            } else {
+                target[p] = arr[i][p];
+            }
+        }
+    }
 }
