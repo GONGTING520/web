777499cf21a3a1e496e0eb85bff24be529bad7c5
GONGTING520 <954160311@qq.com>
2017-11-26 10:00:47 +0800

    增加能保存皮肤颜色的功能

--- jQuery练习/7_网上商城/common.js
+++ jQuery练习/7_网上商城/common.js
@@ -1,4 +1,9 @@
 $(function () {
+    // 首先先加载cookie中的肤色
+    var sCookieColor = $.cookie('skin');
+    if (sCookieColor) {
+        changeSkin(sCookieColor, $('#skin .' + sCookieColor));
+    }
     // 搜索框部分开始
     $('#search').on('focus', function () {
         if (this.value == this.defaultValue) {
@@ -17,10 +22,23 @@ $(function () {
         e = e || window.event;
         var target = e.target || e.srcElement;
         if (target != this) {
-            $('#nav').get(0).className = target.className;
-            $('#shopping-list h2').get(0).className = target.className + ' list-title';
-            $(target).addClass('selected').siblings().removeClass('selected');
+            var date = new Date();
+            date.setDate(date.getDate() + 30);
+            var sExpires = "expires=" + date.toUTCString();
+            var color = target.className.split(' ')[0];
+            changeSkin(color, target);
+            $.cookie('skin', color, sExpires);
         }
     });
+
+    /**
+     * 切换皮肤颜色
+     * 传入切换的颜色color，改变对号的elem
+     * */
+    function changeSkin(color, elem) {
+        $('#nav').get(0).className = color;
+        $('#shopping-list h2').get(0).className = color + ' list-title';
+        $(elem).addClass('selected').siblings().removeClass('selected');
+    }
     // 换肤部分结束
 });
