43526cbfc1b85fb98f121470011daaa59aacd22e
GONGTING520 <954160311@qq.com>
2017-12-01 15:34:48 +0800

    将方块从两个改为四个方块

--- tetris/diamonds.js
+++ tetris/diamonds.js
@@ -2,34 +2,34 @@ var $gameContent = $('#game .game-content');
 // 定义一个父类
 function Common() {
     this.iWidth = $gameContent.width() / 10;
-    this.iHeight = this.iWidth * 2;
-    this.$Div1 = $('<div></div>');
-    this.$Div2 = $('<div></div>');
     this.aDiv = [];
-    for (var i = 0; i < 4; i++) {
+    for (var i = 0; i < 4; i++) { //定义四个小方块
         this.aDiv.push($('<div></div>').get(0));
+        this.aDiv[i].style.width = this.iWidth + 'px';
+        this.aDiv[i].style.height = this.iWidth + 'px';
     }
     this.sDirction = getRandom(1, 2) == 1 ? 'left' : 'right'; //获取方向
 }
 // 生成一个形状类似于7的方块
 function Seven() {
     Common.call(this); //继承父类的属性
-    console.log(this.aDiv);
-    this.$Div1.add(this.$Div2).addClass('seven'); //给两个块添加一个类名
-    var iLeft = this.sDirction === 'left' ? 150 : 120;
-    this.$Div1.css({
-        width: this.iHeight,
-        height: this.iWidth,
-        top: -90,
-        left: 120
+    var sDir = this.sDirction;
+    var iWid = this.iWidth;
+    $(this.aDiv).each(function (index) {
+        this.className = 'seven'; //给每一块都添加一个类名
+        if (index < 2) { //若是前两个  0和1
+            $(this).css({
+                top: -3 * iWid,
+                left: (index + 4) * iWid
+            });
+        } else {
+            $(this).css({
+                top: (index - 4) * iWid
+            });
+            sDir === 'left' ? $(this).css('left', 4 * iWid) : $(this).css('left', 5 * iWid);
+        }
+        $gameContent.append($(this));
     });
-    this.$Div2.css({
-        width: this.iWidth,
-        height: this.iHeight,
-        top: -60,
-        left: iLeft
-    });
-    $gameContent.append(this.$Div1).append(this.$Div2);
 }
 
 
@@ -38,15 +38,22 @@ function Seven() {
 // 生成一个四方形的方块
 function Diamond() {
     Common.call(this);
-    this.$Div1.addClass('diamond');
-    this.$Div2.remove(); //移除生成的第二个元素，因为只需要一个
-    this.$Div1.css({
-        width: this.iHeight,
-        height: this.iHeight,
-        top: -60,
-        left: 120
+    var iWid = this.iWidth;
+    $(this.aDiv).each(function (index) {
+        this.className = 'diamond'; //给每一块都添加一个类名
+        if (index < 2) { //若是前两个  0和1
+            $(this).css({
+                top: -2 * iWid,
+                left: (index + 4) * iWid
+            });
+        } else {
+            $(this).css({
+                top: -iWid,
+                left: (index + 2) * iWid
+            });
+        }
+        $gameContent.append($(this));
     });
-    $gameContent.append(this.$Div1);
 }
 
 
@@ -54,22 +61,23 @@ function Diamond() {
 // 生成一个类似闪电形状或“z形”的方块
 function DiamondZ() {
     Common.call(this);
-    this.$Div1.add(this.$Div2).addClass('diamond-z');
-    var iLeft = this.sDirction === 'left' ? 150 : 120;
-    this.$Div1.css({
-        width: this.iWidth,
-        height: this.iHeight,
-        top: -90,
-        left: 120
+    var sDir = this.sDirction;
+    var iWid = this.iWidth;
+    $(this.aDiv).each(function (index) {
+        this.className = 'diamond-z'; //给每一块都添加一个类名
+        if (index < 2) { //若是前两个  0和1
+            $(this).css({
+                top: -(3 - index) * iWid,
+                left: (sDir === 'left' ? 4 : 5) * iWid
+            });
+        } else {
+            $(this).css({
+                top: -(4 - index) * iWid,
+                left: (sDir === 'left' ? 5 : 4) * iWid
+            });
+        }
+        $gameContent.append($(this));
     });
-    this.$Div2.css({
-        width: this.iWidth,
-        height: this.iHeight,
-        top: -60,
-        left: iLeft
-    });
-    iLeft === 120 ? this.$Div1.css('left', 150) : null;
-    $gameContent.append(this.$Div1).append(this.$Div2);
 }
 
 
@@ -77,20 +85,22 @@ function DiamondZ() {
 // 生成一个类似三角形状的方块
 function Triangle() {
     Common.call(this);
-    this.$Div1.add(this.$Div2).addClass('triangle');
-    this.$Div1.css({
-        width: this.iWidth,
-        height: this.iWidth,
-        top: -60,
-        left: 120
+    var iWid = this.iWidth;
+    $(this.aDiv).each(function (index) {
+        this.className = 'triangle'; //给每一块都添加一个类名
+        if (index === 0) { //若是第一个
+            $(this).css({
+                top: -2 * iWid,
+                left: 4 * iWid
+            });
+        } else {
+            $(this).css({
+                top: -iWid,
+                left: (index + 2) * iWid
+            });
+        }
+        $gameContent.append($(this));
     });
-    this.$Div2.css({
-        width: this.iWidth + this.iHeight,
-        height: this.iWidth,
-        top: -30,
-        left: 90
-    });
-    $gameContent.append(this.$Div1).append(this.$Div2);
 }
 
 
@@ -99,13 +109,13 @@ function Triangle() {
 // 生成一条直线的方块
 function Line() {
     Common.call(this);
-    this.$Div1.add(this.$Div2).addClass('line');
-    this.$Div2.remove();
-    this.$Div1.css({
-        width: this.iWidth,
-        height: this.iWidth * 4,
-        top: -120,
-        left: 120
+    var iWid = this.iWidth;
+    $(this.aDiv).each(function (index) {
+        this.className = 'line'; //给每一块都添加一个类名
+        $(this).css({
+            top: -(index + 1) * iWid,
+            left: 4 * iWid
+        });
+        $gameContent.append($(this));
     });
-    $gameContent.append(this.$Div1);
 }
