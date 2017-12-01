7af1f786b9eef608e53e41cf8e5db63cc92d50f4
GONGTING520 <954160311@qq.com>
2017-12-01 23:30:01 +0800

    增加了方块左右移动的功能，但是未做碰壁检测

--- tetris/index.js
+++ tetris/index.js
@@ -1,5 +1,30 @@
-// var d = new Diamond();
 // var a = new Seven();
+// a.fallDown();
+// var b = new Diamond();
 // var c = new DiamondZ();
-// var b = new Triangle();
+// var d = new Triangle();
 // var e = new Line();
+var a = newTetris();
+a.fallDown();
+document.onkeyup = function (e) {
+    switch (e.keyCode) {
+        case 37: //左
+            $(a.aDiv).each(function () {
+                var iLeft = $(this).css('left').split('px')[0] - $(this).width();
+                $(this).css('left', iLeft);
+            });
+            break;
+        case 38: //上
+            console.log('up');
+            break;
+        case 39: //右
+            $(a.aDiv).each(function () {
+                var iLeft = parseInt($(this).css('left').split('px')[0]) + $(this).width();
+                $(this).css('left', iLeft);
+            });
+            break;
+        case 40: //下
+            console.log('down');
+            break;
+    }
+}
