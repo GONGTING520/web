700e8f33189b97c3629bc1cac48e7423877444ec
GONGTING520 <954160311@qq.com>
2017-12-02 20:30:16 +0800

    添加定时器，让方块一直下落，当到底部或者碰到其他方块时停下

--- tetris/index.js
+++ tetris/index.js
@@ -4,16 +4,48 @@
 // var c = new DiamondZ();
 // var d = new Triangle();
 // var e = new Line();
-var a = newTetris();
-a.fallDown();
+var $gameNext = $('#game .next');
+var first = newTetris();
+var b = newTetris();
+var $next = $(b.aDiv).clone();
+addNext($next);
+// first.fallDown();
+fall(first);
+
+/**
+ * 拷贝传入的对象，在下一个位置上，插入这个表示下一个会出现的方块的对象
+ * 
+ * @param {any} $next 表示下一个会出现的方块
+ */
+function addNext(next) {
+    next.each(function (index, elem) {
+        $(this).css({
+            top: '+=' + $(elem).width() * 3
+        });
+    });
+    $gameNext.append(next);
+}
+
+function fall(elem){
+    elem.timer = setInterval(function () {
+        elem.fallDown();
+        if (elem.collision()) {
+            clearInterval(elem.timer);
+            elem.bMovable = false;
+        }
+    }, elem.speed);
+}
+
+
 document.onkeyup = function (e) {
     switch (e.keyCode) {
         case 37: //左
-            // a活着，并且可以左移
-            if (a.bMovable && a.bLeftMovable) {
-                $(a.aDiv).each(function () {
-                    var iLeft = $(this).css('left').split('px')[0] - $(this).width();
-                    $(this).css('left', iLeft);
+            // first活着，并且可以左移
+            if (first.bMovable && first.bLeftMovable) {
+                $(first.aDiv).each(function (index, elem) {
+                    $(this).css({
+                        left: '-=' + $(elem).width()
+                    });
                 });
             }
             break;
@@ -21,11 +53,12 @@ document.onkeyup = function (e) {
             console.log('up');
             break;
         case 39: //右
-            // a活着，并且可以右移
-            if (a.bMovable && a.bRightMovable) {
-                $(a.aDiv).each(function () {
-                    var iLeft = parseInt($(this).css('left').split('px')[0]) + $(this).width();
-                    $(this).css('left', iLeft);
+            // first活着，并且可以右移
+            if (first.bMovable && first.bRightMovable) {
+                $(first.aDiv).each(function (index, elem) {
+                    $(this).css({
+                        left: '+=' + $(elem).width()
+                    });
                 });
             }
             break;
