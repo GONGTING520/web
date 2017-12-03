32743286acb28037165defef77ebea92d129fe94
GONGTING520 <954160311@qq.com>
2017-12-03 13:50:40 +0800

    增加可以判断下落对象是否与下落完毕的数组中的对象碰撞的方法，并在定时器中调用

--- tetris/index.js
+++ tetris/index.js
@@ -4,10 +4,10 @@ var $gameContent = $('.game-content', $game);
 var aTetris = []; //表示已经下落停止的方块数组
 var first = newTetris();
 var now = first; //当前下落的方块
-var b = newTetris();
-var $next = $(b.aDiv).clone();
-addNext($next);
 fallDown(first);
+var oNext = newTetris(); //下一个下落的方块
+var $next = $(oNext.aDiv).clone();
+addNext($next);
 
 /**
  * 拷贝传入的对象，在下一个位置上，插入这个表示下一个会出现的方块的对象
@@ -15,7 +15,7 @@ fallDown(first);
  * @param {any} $next 表示下一个会出现的方块
  */
 function addNext(next) {
-    $('div', $gameNext).remove();
+    $('div', $gameNext).remove(); //移除原来的方块
     next.each(function (index, elem) {
         $(this).css({
             top: '+=' + $(elem).width() * 3
@@ -31,7 +31,10 @@ function addNext(next) {
 function fallDown(elem) {
     elem.timer = setInterval(function () {
         elem.fallOne();
-        if (elem.collision()) {
+        // var i = elem.collisionArrElem(aTetris);
+        // console.log(i);
+        // console.log(aTetris);
+        if (elem.collisionBottom() || collision(elem)) {
             clearInterval(elem.timer);
             elem.bMovable = false;
             aTetris.push(now);
@@ -39,12 +42,28 @@ function fallDown(elem) {
         }
     }, elem.speed);
 }
-
+/**
+ * 判断正在下落的对象elem与已经下落完毕的对象数组aTetris中的对象是否碰撞
+ * 
+ * @param {any} elem 正在下落的对象
+ * @returns bFlag 表示是否停止，true表示停止，false表示不停止
+ */
+function collision(elem) {
+    var bFlag = false;
+    for (var i = 0; i < aTetris.length; i++) {
+        bFlag = elem.collisionArrElem(aTetris[i]);
+    }
+    return bFlag;
+}
+/**
+ * 切换当前控制的元素和下一个方块元素
+ * 
+ */
 function toggleNowAndNext() {
-    now = b;
-    fallDown(b);
-    b = newTetris();
-    $next = $(b.aDiv).clone();
+    now = oNext;
+    fallDown(oNext);
+    oNext = newTetris();
+    $next = $(oNext.aDiv).clone();
     addNext($next);
 }
 /**
@@ -74,10 +93,10 @@ function overEdge(elem, edgeLeft, sLOrR) {
 }
 
 document.onkeydown = function (e) {
-    switch (e.keyCode) {
-        case 37: //左
-            // first活着，并且可以左移
-            if (now.bMovable) {
+    // first活着
+    if (now.bMovable) {
+        switch (e.keyCode) {
+            case 37: //左
                 // 如果没与左边界碰撞
                 if (!overEdge(now.aDiv, $gameContent.css('left').split('px')[0], 'left')) {
                     $(now.aDiv).each(function (index, elem) {
@@ -92,14 +111,11 @@ document.onkeydown = function (e) {
                         });
                     });
                 }
-            }
-            break;
-        case 38: //上
-            console.log('up');
-            break;
-        case 39: //右
-            // first活着，并且可以右移
-            if (now.bMovable) {
+                break;
+            case 38: //上
+                console.log('up');
+                break;
+            case 39: //右
                 // 如果没与右边界碰撞
                 var iRight = parseInt($gameContent.css('left').split('px')[0]) + $gameContent.width() - now.iWidth;
                 if (!overEdge(now.aDiv, iRight, 'right')) {
@@ -115,10 +131,16 @@ document.onkeydown = function (e) {
                         });
                     });
                 }
-            }
-            break;
-        case 40: //下
-            console.log('down');
-            break;
+                break;
+            case 40: //下
+                if (!now.collisionBottom()) {
+                    $(now.aDiv).each(function (index, elem) {
+                        $(this).css({
+                            top: '+=' + $(elem).height()
+                        });
+                    });
+                }
+                break;
+        }
     }
 }
