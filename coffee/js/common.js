ec5e2853bca9b31fb3714d388f794b7b688d2e47
GONGTING520 <954160311@qq.com>
2017-12-20 23:12:35 +0800

    咖啡网站的公共js交互

---
+++ coffee/js/common.js
@@ -0,0 +1,20 @@
+$(function () {
+    var $author = $('#author');
+    var $authorContainer = $('.authorContainer',$author);
+    var $authorInfo = $('.info',$authorContainer);
+    var $authorLeader = $('.group-leader',$authorInfo);
+    var $authorLeaderName = $('.leader',$authorInfo);
+    var $authorGroup = $('.member',$authorInfo);
+    var $authorGroupName = $('.group',$authorInfo);
+    $author.hover(function () {
+        $authorContainer.stop().show(500);
+    },function () {
+        $authorContainer.stop().hide(500);
+    });
+    $authorGroup.on('click',function () {
+        $authorGroupName.toggle(500);
+    });
+    $authorLeader.on('click',function () {
+        $authorLeaderName.toggle(300);
+    });
+});
