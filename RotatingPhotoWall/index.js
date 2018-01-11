9666ca66cc4b7e865a60713a646a5256bb6bbac3
GONGTING520 <954160311@qq.com>
2018-01-11 17:36:33 +0800

    实现点击任意一张小图片可以切到相应图片并合并，再点击返回原状态的效果

--- RotatingPhotoWall/index.js
+++ RotatingPhotoWall/index.js
@@ -2,7 +2,9 @@ const ROW = 4,
     COL = 6,
     NUM = ROW * COL; //定义常量
 let aDiv = null;
+let aSpan = null;
 document.body.loadSuccessCount = 0; //定义加载好的图片的数量
+document.body.bFlag = true; //true表示处于散开状态，false表示处于合并状态
 
 //图片预加载
 for (let i = 0; i < NUM; i++) {
@@ -17,8 +19,6 @@ for (let i = 0; i < NUM; i++) {
     };
     oSmallImg.src = 'img/thumbs/' + (i + 1) + '.jpg';
     oBigImg.src = 'img/' + (i + 1) + '.jpg';
-    //让每个小图片的旋转角度随机
-    // oSmallImg.style.transform = 'rotate(' + parseInt(Math.random() * 60 - 30) + 'deg)';
 }
 
 function show() {
@@ -26,22 +26,77 @@ function show() {
         let oDiv = document.createElement('div');
         oDiv.className = 'img';
         oDiv.style.background = 'url(img/thumbs/' + (i + 1) + '.jpg)';
+        oDiv.innerHTML = '<span></span>'
         document.body.appendChild(oDiv);
     }
 
     aDiv = document.getElementsByTagName('div');
+    aSpan = document.getElementsByTagName('span');
     //计算小图片的水平的间隙iColGap和垂直的间隙iRowGap
     let iColGap = (document.body.offsetWidth - COL * aDiv[0].offsetWidth) / (COL + 1);
     let iRowGap = (document.body.offsetHeight - ROW * aDiv[0].offsetWidth) / (ROW + 1);
+    let iBigColGap = (document.body.offsetWidth - COL * (aDiv[0].offsetWidth - 8)) / 2;
+    let iBigRowGap = (document.body.offsetHeight - ROW * (aDiv[0].offsetWidth - 8)) / 2;
     for (let i = 0; i < NUM; i++) {
         //小图片最开始的位置
         aDiv[i].pos = {
             top: iRowGap + parseInt(i / 6) * (iRowGap + aDiv[0].offsetWidth),
             left: iColGap + (i % 6) * (iColGap + aDiv[0].offsetWidth)
         };
-        aDiv[i].style.top = aDiv[i].pos.top + 'px';
-        aDiv[i].style.left = aDiv[i].pos.left + 'px';
+        //小图片合并后的位置
+        aDiv[i].newPos = {
+            top: iBigRowGap + parseInt(i / 6) * (aDiv[0].offsetWidth - 8),
+            left: iBigColGap + (i % 6) * (aDiv[0].offsetWidth - 8)
+        };
+        aDiv[i].index = i; //设置索引值
+        //让每个小图片的旋转角度随机
+        movePos(i);
         //设置每张小图片飞出的延迟时间
         aDiv[i].style.transitionDelay = (NUM - i) * 100 + 'ms';
     }
+}
+
+document.body.onclick = function (e) {
+    let oTarget = e.target;
+    if (oTarget != e.currentTarget) {
+        //说明不是空白部分
+        if (oTarget.tagName === 'SPAN') {
+            //说明是span
+            oTarget = oTarget.offsetParent; //将targer改为div
+        }
+        if (document.body.bFlag) {
+            //说明处于散开状态
+            for (let i = 0; i < NUM; i++) {
+                //改变span的背景图片，并让透明度为1
+                aSpan[i].style.background = 'url(img/' + oTarget.index + '.jpg)';
+                aSpan[i].style.opacity = 1;
+                //让所有的小图片旋转角度为0，并定位到新得位置
+                aDiv[i].style.transform = 'rotate(0deg)';
+                aDiv[i].style.top = aDiv[i].newPos.top + 'px';
+                aDiv[i].style.left = aDiv[i].newPos.left + 'px';
+                aDiv[i].style.border = '1px solid #ffffff';
+                aDiv[i].style.transitionDelay = '0ms';
+            }
+        } else {
+            //说明处于合并状态
+            for (let i = 0; i < NUM; i++) {
+                movePos(i);
+                aSpan[i].style.opacity = 0;
+                aDiv[i].style.border = '5px solid #ffffff';
+                aDiv[i].style.transitionDelay = '0ms';
+            }
+        }
+        document.body.bFlag = !document.body.bFlag;
+    }
+};
+
+/**
+ * 让每个小图片的旋转角度随机，并定位到pos的位置
+ * 
+ * @param {number} i 第几个div元素
+ */
+function movePos(i) {
+    aDiv[i].style.transform = 'rotate(' + parseInt(Math.random() * 60 - 30) + 'deg)';
+    aDiv[i].style.top = aDiv[i].pos.top + 'px';
+    aDiv[i].style.left = aDiv[i].pos.left + 'px';
 }
