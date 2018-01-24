f925409fd736849f0dcc7a916085cbd0e2cb17d7
GONGTING520 <954160311@qq.com>
2018-01-24 17:45:25 +0800

    新增css自动增加前缀，babel，以及watch的设置

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,5 +1,9 @@
 const path = require('path');
 const HtmlPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const glob = require('glob');
+const PurifyCSSPlugin = require('purifycss-webpack');
 
 module.exports = {
     entry: {
@@ -8,7 +12,8 @@ module.exports = {
     },
     output: {
         filename: '[name].js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: 'http://127.0.0.1:8888/'
     },
     devServer: {
         contentBase: './dist/index.html',
@@ -23,6 +28,64 @@ module.exports = {
             },
             hash: true,
             template: './src/index.html'
-        })
-    ]
+        }),
+        new UglifyJsPlugin(),
+        new ExtractTextPlugin('css/index.css'),
+        new PurifyCSSPlugin({
+            paths: glob.sync(path.join(__dirname, 'src/*.html')),
+        }),
+    ],
+    module: {
+        rules: [{
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [{
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1
+                        }
+                    }, 'postcss-loader']
+                })
+            },
+            {
+                test: /\.(jpg|png|gif)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 500,
+                        outputPath: 'img/'
+                    }
+                }]
+            },
+            {
+                test: /\.(html|htm)$/i,
+                use: [{
+                    loader: 'html-withimg-loader'
+                }]
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+            {
+                test: /\.(js|jsx)$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env', 'react']
+                    }
+                },
+                exclude: /node_modules/
+            },
+        ]
+    },
+    watchOptions: {
+        poll: 1000,
+        aggregateTimeout: 500,
+        ignored: /node_modules/
+    }
 };
