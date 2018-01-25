5b31c65c7df60f101c8a735f45554fac38106e57
GONGTING520 <954160311@qq.com>
2018-01-25 16:13:01 +0800

    对webpack配置的测试文件

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -2,7 +2,9 @@ import _ from 'lodash';
 import css from './css/index.css';
 import css1 from './css/index2.css';
 import scss from './css/index3.scss';
+// import $ from 'jquery';
 
+let json = require('../author.json');
 let elem = document.createElement('div');
-elem.innerHTML = 'hello webpack';
-document.body.appendChild(elem);
+elem.innerHTML = `author:${json.name},age:${json.age},school:${json.school}`;
+$(document.body).append(elem);
