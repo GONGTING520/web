5d63c650f0d973a4687ed40f70df0e5355f6bca3
GONGTING520 <954160311@qq.com>
2018-01-29 15:10:24 +0800

    新增编程式导航的功能

--- vue/vue-cli/src/App.vue
+++ vue/vue-cli/src/App.vue
@@ -2,6 +2,11 @@
   <div id="app">
     <img src="./assets/logo.png">
     <div>
+      <button @click="home">首页</button>
+      <button @click="back">后退</button>
+      <button @click="forward">前进</button>
+    </div>
+    <div>
       <router-link to="/">首页</router-link>
       <router-link to="/test">主路由test</router-link>
       <router-link :to="{name:'test1', params:{username:'gongting', age:18}}">子路由test1传对象</router-link>
@@ -15,7 +20,20 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  methods: {
+    home () {
+      this.$router.push('/')
+      // this.$router.push('/test/test2/6/zhangsan')
+      // this.$router.push({name:'test1', params:{username:'gongting', age:18}})
+    },
+    back () {
+      this.$router.go(-1)
+    },
+    forward () {
+      this.$router.go(1)
+    }
+  }
 }
 </script>
 
