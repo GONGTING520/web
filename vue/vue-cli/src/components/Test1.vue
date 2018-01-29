9b3883bd803916ffb41b39d2387e718dbef8cacf
GONGTING520 <954160311@qq.com>
2018-01-29 14:23:41 +0800

    在自定义的子路由中使用获取router-link传来的参数，包括传对象和传url

---
+++ vue/vue-cli/src/components/Test1.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <h1>子路由test1传对象</h1>
+      <h1 v-text="$route.params.username"></h1>
+      <h1 v-text="$route.params.age"></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue/vue-cli/src/components/Test2.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>子路由test2传URL</h1>
+    <h1>{{ $route.params.userId }}</h1>
+    <h1>{{ $route.params.username }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
