90af0b54721e166f665a44993a4b7f2c5cb3a6fa
GONGTING520 <954160311@qq.com>
2018-01-29 14:21:56 +0800

    在自定义的组件中使用子路由

---
+++ vue/vue-cli/src/components/Test.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>主路由test</h1>
+        <h1 v-text="msg"></h1>
+        <router-view />
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'test component'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: aqua;
+}
+</style>
