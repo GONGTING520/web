2728bd18499a44e2723ee30b065139e8d14edf6c
GONGTING520 <954160311@qq.com>
2018-01-29 15:09:20 +0800

    给test主路由下的子路由设置过渡动画，并在进入和跳出此路由时执行相应的钩子函数

--- vue/vue-cli/src/components/Test.vue
+++ vue/vue-cli/src/components/Test.vue
@@ -2,7 +2,9 @@
     <div>
         <h1>主路由test</h1>
         <h1 v-text="msg"></h1>
-        <router-view />
+        <transition name="fade">
+          <router-view />
+        </transition>
     </div>
 </template>
 
@@ -12,6 +14,16 @@ export default {
     return {
       msg: 'test component'
     }
+  },
+  beforeRouteEnter: (to, from, next) => {
+    console.log(to)
+    console.log(from)
+    next()
+    // next(false)
+  },
+  beforeRouteLeave: (to, from, next) => {
+    console.log('route leave')
+    next()
   }
 }
 </script>
@@ -19,5 +31,23 @@ export default {
 <style scoped>
 h1 {
   color: aqua;
+}
+.fade-enter {
+  opacity: 0;
+}
+.fade-enter-to {
+  opacity: 1;
+}
+.fade-enter-active {
+  transition: opacity .5s ease;
+}
+.fade-leave {
+  opacity: 1;
+}
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-leave-active {
+  transition: opacity .5s ease;
 }
 </style>
