4003688ae41c0b87f211d049dcfdd9cc131a8811
GONGTING520 <954160311@qq.com>
2018-01-29 14:26:22 +0800

    使用router-link来跳转组件，并使用传对象和传url的方式传参数

---
+++ vue/vue-cli/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div>
+      <router-link to="/">首页</router-link>
+      <router-link to="/test">主路由test</router-link>
+      <router-link :to="{name:'test1', params:{username:'gongting', age:18}}">子路由test1传对象</router-link>
+      <router-link to="/test/test2/6/zhangsan">子路由test2传URL</router-link>
+      <router-link to="/home">重定位</router-link>
+      <router-link to="/abc">alias</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
