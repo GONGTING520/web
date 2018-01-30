efa845237bb2a4f09332fe590d3a95da48cb0309
GONGTING520 <954160311@qq.com>
2018-01-30 17:21:27 +0800

    定义头部组件，需要传入标题titile和背景颜色

---
+++ VueApp/src/components/common/Header.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="header" :style="{backgroundColor: bgColor}">
+        <span @click="$emit('tapLi', 0)">
+            <router-link to="/" class="home" :style="{backgroundColor: bgColor}">首页</router-link>
+        </span>
+        <span class="title">
+            <slot>default</slot>
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+  props: ["bgColor"]
+};
+</script>
+
+<style scoped>
+.header {
+  height: 1rem;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  line-height: 1rem;
+  color: #ffffff;
+}
+.home {
+  color: #ffffff;
+  margin-left: 0.2rem;
+  font-size: 0.3rem;
+  outline: none;
+}
+.title {
+  font-size: 0.5rem;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
