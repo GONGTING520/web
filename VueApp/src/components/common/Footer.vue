e4078d8bdd1a9a844626ca84190739b70d0f7dbc
GONGTING520 <954160311@qq.com>
2018-01-30 17:20:59 +0800

    定义脚部组件，当点击相应选项时向组件外抛出一个事件

---
+++ VueApp/src/components/common/Footer.vue
@@ -0,0 +1,46 @@
+<template>
+  <ul class="footer" :style="{backgroundColor: bgColor}">
+      <li @click="$emit('tapLi', 0)">
+          <router-link to="/">电影</router-link>
+      </li>
+      <li @click="$emit('tapLi', 1)">
+          <router-link to="/music">音乐</router-link>
+      </li>
+      <li @click="$emit('tapLi', 2)">
+          <router-link to="/book">书籍</router-link>
+      </li>
+      <li @click="$emit('tapLi', 3)">
+          <router-link to="/photo">图片</router-link>
+      </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ["bgColor"]
+};
+</script>
+
+<style scoped>
+.footer {
+  display: flex;
+  position: fixed;
+  bottom: 0;
+  height: 1rem;
+  width: 100%;
+  line-height: 1rem;
+}
+.footer li {
+  flex-grow: 1;
+  text-align: center;
+}
+.footer li a {
+  color: #cccccc;
+  font-size: 0.3rem;
+  display: block;
+  outline: none;
+}
+.footer li a.router-link-exact-active {
+  color: #ffffff;
+}
+</style>
