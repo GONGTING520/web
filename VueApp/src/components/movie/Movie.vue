d389fed6520cde351ad1c7fcc1b666d3c1aad7d9
GONGTING520 <954160311@qq.com>
2018-01-30 17:22:17 +0800

    定义电影部分组件，并用数组模拟数据生成电影列表并设置样式

---
+++ VueApp/src/components/movie/Movie.vue
@@ -0,0 +1,50 @@
+<template>
+    <!-- <ul> -->
+        <!-- <common-header bgColor="rgb(33, 150, 243)">
+            <span>Movie</span>
+        </common-header> -->
+        <ul class="container">
+            <li v-for="val in arr" v-text="val.index"></li>
+        </ul>
+        <!-- <common-footer bgColor="rgb(33, 150, 243)"></common-footer> -->
+    <!-- </div> -->
+</template>
+
+<script>
+import commonHeader from "@/components/common/Header";
+import commonFooter from "@/components/common/Footer";
+export default {
+  data() {
+    return {
+      arr: [
+        {
+          index: 1
+        },
+        {
+          index: 2
+        },
+        {
+          index: 3
+        },
+        {
+          index: 4
+        },
+        {
+          index: 5
+        }
+      ]
+    };
+  },
+  components: {
+    commonHeader,
+    commonFooter
+  }
+};
+</script>
+
+<style scoped>
+.container li {
+  height: 3rem;
+  border-bottom: 1px solid;
+}
+</style>
