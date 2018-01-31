f1266f5852b9e572a71c962cd7da6bce262b1a5c
GONGTING520 <954160311@qq.com>
2018-01-31 11:54:42 +0800

    使用传递name 的方式来判断使用什么颜色和title

--- VueApp/src/App.vue
+++ VueApp/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <common-header :bgColor="bgColor" @tapLi="change">
-      <span v-text="headerTitle"></span>
+    <common-header :bgColor="bgColor" @tapLi="change('movie')">
+      <span v-text="title"></span>
     </common-header>
     <router-view/>
     <common-footer @tapLi="change" :bgColor="bgColor"></common-footer>
@@ -16,42 +16,69 @@ export default {
   name: "App",
   data() {
     return {
-      headAndFoot: [
-        {
-          color: "rgb(33, 150, 243)",
-          title: "Movie"
-        },
-        {
-          color: "rgb(0, 130, 112)",
-          title: "Music"
-        },
-        {
-          color: "rgb(121, 85, 72)",
-          title: "Book"
-        },
-        {
-          color: "rgb(63, 81, 181)",
-          title: "Photo"
-        }
-      ],
-      index: 0
+      bgColor: "rgb(33, 150, 243)",
+      title: "Movie",
+      // headAndFoot: [
+      //   {
+      //     color: "rgb(33, 150, 243)",
+      //     title: "Movie",
+      //     name: "movie"
+      //   },
+      //   {
+      //     color: "rgb(0, 130, 112)",
+      //     title: "Music",
+      //     name: "music"
+      //   },
+      //   {
+      //     color: "rgb(121, 85, 72)",
+      //     title: "Book",
+      //     name: "book"
+      //   },
+      //   {
+      //     color: "rgb(63, 81, 181)",
+      //     title: "Photo",
+      //     name: "photo"
+      //   }
+      // ],
+      // index: 0
     };
   },
   computed: {
-    bgColor() {
-      return this.headAndFoot[this.index].color;
-    },
-    headerTitle() {
-      return this.headAndFoot[this.index].title;
-    }
+    // bgColor() {
+    //   return this.headAndFoot[this.index].color;
+    // },
+    // headerTitle() {
+    //   return this.headAndFoot[this.index].title;
+    // }
   },
   components: {
     commonHeader,
     commonFooter
   },
   methods: {
-    change(idx) {
-      this.index = idx;
+    // change(idx) {
+    //   this.index = idx;
+    // },
+    change(tapName) {
+      switch (tapName) {
+        case "movie":
+          this.bgColor = "rgb(33, 150, 243)";
+          this.title = "Movie";
+          break;
+        case "music":
+          this.bgColor = "rgb(0, 130, 112)";
+          this.title = "Music";
+          break;
+        case "book":
+          this.bgColor = "rgb(121, 85, 72)";
+          this.title = "Book";
+          break;
+        case "photo":
+          this.bgColor = "rgb(63, 81, 181)";
+          this.title = "Photo";
+          break;
+        default:
+      }
     }
   }
 };
@@ -64,6 +91,7 @@ export default {
   bottom: 1rem;
   left: 0;
   right: 0;
-  overflow: scroll;
+  font-size: 0.3rem;
+  overflow-y: scroll;
 }
 </style>
