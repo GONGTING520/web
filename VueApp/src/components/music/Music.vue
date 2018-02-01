76cf0e70fc97b46a52593663f6bda389c0110bf3
GONGTING520 <954160311@qq.com>
2018-02-01 11:53:22 +0800

    在音乐组件中调用音乐接口来获取音乐信息，并设置音乐信息的样式

--- VueApp/src/components/music/Music.vue
+++ VueApp/src/components/music/Music.vue
@@ -1,65 +1,51 @@
 <template>
-    <!-- <div> -->
-        <!-- <common-header bgColor="rgb(0, 130, 112)">
-            <span>Music</span>
-        </common-header> -->
-        <div class="container">
-            <div class="albumInfo"></div>
-            <ul>
-                <li v-for="obj in arr" class="musicInfo" :class="{selected: obj.isSelected}" @click="showMusic(obj)">
-                    <span v-text="obj.name"></span>
-                    <span v-text="obj.singer"></span>
-                    <span v-text="obj.time"></span>
-                </li>
-            </ul>
-        </div>
-        <!-- <common-footer bgColor="rgb(0, 130, 112)"></common-footer> -->
-    <!-- </div> -->
+  <div class="container">
+    <div class="albumInfo">
+      <div class="album-left">
+        <img :src="pic_small">
+      </div>
+      <div class="alubum-right">
+        <p v-text="name" class="title"></p>
+        <p v-text="singer" class="singer"></p>
+        <audio class="controler" :src="audio" controls></audio>        
+      </div>
+      <img :src="pic_small" class="blur">
+    </div>
+    <div class="lrc"></div>
+    <ul>
+      <li v-for="obj in musicList" :key="obj.id" class="musicInfo" :class="{selected: obj.isSelected}" @click="showMusic(obj)">
+        <span v-text="obj.album_title"></span> - 
+        <span v-text="obj.artist_name"></span>
+      </li>
+    </ul>
+    <div class="loading" v-show="isLoading">
+      <img src="../../assets/img/loadingMusicList.gif" alt="">
+    </div>
+  </div>
 </template>
 
 <script>
-import commonHeader from "@/components/common/Header";
-import commonFooter from "@/components/common/Footer";
+import axios from "axios";
 export default {
   data() {
     return {
-      arr: [
-        {
-          name: "成都",
-          singer: "zzz",
-          time: "5:00",
-          isSelected: false
-        },
-        {
-          name: "我要你",
-          singer: "aer",
-          time: "4:00",
-          isSelected: false
-        },
-        {
-          name: "又",
-          singer: "rehy",
-          time: "5:10",
-          isSelected: false
-        },
-        {
-          name: "时间煮雨",
-          singer: "ewt",
-          time: "3:00",
-          isSelected: false
-        },
-        {
-          name: "假如爱有天意",
-          singer: "eiwah",
-          time: "2:45",
-          isSelected: false
-        }
-      ]
+      musicList: [],
+      isLoading: true,
+      iNow: null,
+      name: "",
+      singer: "",
+      pic_small: "",
+      audio: ""
     };
   },
-  components: {
-    commonHeader,
-    commonFooter
+  watch: {
+    iNow() {
+      this.name = this.musicList[this.iNow].album_title;
+      this.singer = this.musicList[this.iNow].artist_name;
+      this.pic_small = this.musicList[this.iNow].pic_small;
+      this.audio = this.musicList[this.iNow].pic_radio;
+      console.log(this.audio);
+    }
   },
   methods: {
     showMusic(obj) {
@@ -70,15 +56,87 @@ export default {
           elem.isSelected = false;
         }
       });
+    },
+    getInfo() {
+      // type = 1-新歌榜,2-热歌榜,11-摇滚榜,12-爵士,16-流行,21-欧美金曲榜,22-经典老歌榜,23-情歌对唱榜,24-影视金曲榜,25-网络歌曲榜
+      axios
+        .get(
+          `${API_PROXY}http://tingapi.ting.baidu.com/v1/restserver/ting?format=json&callback=&from=webapp_music&method=baidu.ting.billboard.billList&type=1&size=10&offset=${
+            this.musicList.length
+          }`
+        )
+        .then(res => {
+          console.log(res);
+          this.musicList = this.musicList.concat(res.data.song_list);
+          this.isLoading = false;
+          if (this.musicList.length == 10) {
+            this.iNow = 0;
+            this.getMusic();
+          }
+        })
+        .catch(() => {
+          console.log("error");
+        });
+    },
+    getMusic() {
+      axios
+        .get(
+          `${API_PROXY}http://tingapi.ting.baidu.com/v1/restserver/ting?format=json&callback=&from=webapp_music&method=baidu.ting.song.play&songid=${
+            this.musicList[this.iNow].song_id
+          }`
+        )
+        .then(res => {
+          console.log(JSON.parse(res.data.substring(1, res.data.length - 2)));
+          this.audio = JSON.parse(res.data.substring(1, res.data.length - 2)).bitrate.file_link;
+          console.log(this.audio);
+        })
+        .catch(res => {
+          console.log("error");
+        });
     }
+  },
+  created() {
+    this.getInfo();
   }
 };
 </script>
 
 <style scoped>
 .albumInfo {
-  height: 3rem;
+  padding: 0.3rem;
   border-bottom: 1px solid;
+  display: flex;
+  align-items: center;
+  position: relative;
+  overflow: hidden;
+}
+.blur {
+  filter: blur(0.6rem);
+  position: absolute;
+  top: 0;
+  z-index: -1;
+  width: 100%;
+}
+.album-left {
+  flex-grow: 1;
+  width: 0;
+  margin-right: 0.3rem;
+}
+.album-left img {
+  width: 100%;
+}
+.alubum-right {
+  flex-grow: 3;
+  width: 0;
+}
+.alubum-right .title {
+  font-size: 0.4rem;
+}
+.alubum-right .singer {
+  font-size: 0.3rem;
+}
+.alubum-right .controler {
+  width: 100%;
 }
 .musicInfo {
   height: 1rem;
@@ -88,5 +146,19 @@ export default {
 }
 .selected {
   background: #cccccc;
+}
+.loading {
+  position: absolute;
+  top: 1rem;
+  bottom: 1rem;
+  left: 0;
+  right: 0;
+  background: rgba(255, 255, 255, 0.6);
+}
+.loading img {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
 </style>
