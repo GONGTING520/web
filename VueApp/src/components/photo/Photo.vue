52176fd3ac549e6d28b2b9206713f3da2a0233d9
GONGTING520 <954160311@qq.com>
2018-02-03 17:13:17 +0800

    增加显示大图片的动画特效

--- VueApp/src/components/photo/Photo.vue
+++ VueApp/src/components/photo/Photo.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="container">
-    <ul>
+    <ul class="clearfix">
       <li class="img-li" @click="iNow=obj.id" v-for="obj in photoList" :key="obj.id" :style="{background: 'url(' + obj.src + ') no-repeat center/cover'}">
-        <div class="layup" v-show="obj.id===iNow" @click.stop="iNow=-1">
-          <img :src="obj.srcBig">
-        </div>      
+        <transition name="fade">
+          <div class="layup" v-show="obj.id===iNow" @click.stop="iNow=-1">
+            <img :src="obj.srcBig">            
+          </div>
+        </transition>   
       </li>
     </ul>
   </div>
@@ -53,5 +55,17 @@ export default {
   top: 50%;
   transform: translate(0, -50%);
   width: 100%;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-enter-to,
+.fade-leave {
+  opacity: 1;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
 }
 </style>
