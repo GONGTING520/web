7d2c01d3628eba0aaac0680d9564869424af7215
GONGTING520 <954160311@qq.com>
2018-02-04 16:20:39 +0800

    修改结构，每当点击小图片时，跳转到相应的大图片界面，使用vuex将图片列表信息放到公共仓库store中，以便相应界面获取信息

--- VueApp/src/components/photo/Photo.vue
+++ VueApp/src/components/photo/Photo.vue
@@ -1,33 +1,28 @@
 <template>
   <div class="container">
     <ul class="clearfix">
-      <li class="img-li" @click="iNow=obj.id" v-for="obj in photoList" :key="obj.id" :style="{background: 'url(' + obj.src + ') no-repeat center/cover'}">
-        <transition name="fade">
-          <div class="layup" v-show="obj.id===iNow" @click.stop="iNow=-1">
-            <img :src="obj.srcBig">            
-          </div>
-        </transition>   
-      </li>
+      <li class="img-li" @click="$router.push(`/photo/photodetail/${obj.id}`)" v-for="obj in photoList" :key="obj.id" :style="{background: 'url(' + obj.src + ') no-repeat center/cover'}"></li>
     </ul>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import store from "@/vuex/store.js";
+
 export default {
   data() {
     return {
-      photoList: [],
-      isScorll: true,
-      iNow: -1
+      photoList: []
     };
   },
-  methods: {},
+  store,
   created() {
     axios
       .get("static/json/photo.json")
       .then(res => {
         this.photoList = Array.from(res.data);
+        this.$store.commit("setPhoto", Array.from(res.data));
       })
       .catch(() => {
         console.log("error");
@@ -41,31 +36,5 @@ export default {
   width: 50%;
   padding-top: 50%;
   float: left;
-}
-.img-li .layup {
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background: #000;
-}
-.img-li .layup img {
-  position: absolute;
-  top: 50%;
-  transform: translate(0, -50%);
-  width: 100%;
-}
-.fade-enter,
-.fade-leave-to {
-  opacity: 0;
-}
-.fade-enter-to,
-.fade-leave {
-  opacity: 1;
-}
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s ease;
 }
 </style>
