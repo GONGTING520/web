e79ad363442cb8363552220ef754300da55052cf
GONGTING520 <954160311@qq.com>
2018-02-04 16:21:15 +0800

    配置图片详情界面的信息

--- VueApp/src/router/index.js
+++ VueApp/src/router/index.js
@@ -6,6 +6,7 @@ import Book from '@/components/book/Book';
 import Photo from '@/components/photo/Photo';
 import Anime from '@/components/anime/Anime';
 import MovieDetail from '@/components/movie/MovieDetail';
+import PhotoDetail from '@/components/photo/PhotoDetail';
 
 Vue.use(Router);
 global.API_PROXY = 'https://bird.ioliu.cn/v2/?url=';
@@ -13,42 +14,58 @@ global.API_PROXY = 'https://bird.ioliu.cn/v2/?url=';
 export default new Router({
   mode: 'history',
   routes: [{
-    path: '/',
-    name: 'anime',
-    component: Anime,
-  }, {
-    path: '/movie',
-    name: 'movie',
-    component: Movie,
-    beforeEnter(to, from, next) {
-      // 如果是刷新来的就改成首页，否则就正常跳转
-      from.name === null ? next('/') : next();
+      path: '/',
+      name: 'anime',
+      component: Anime,
+    }, {
+      path: '/movie',
+      name: 'movie',
+      component: Movie,
+      beforeEnter(to, from, next) {
+        // 如果是刷新来的就改成首页，否则就正常跳转
+        from.name === null ? next('/') : next();
+      }
+    }, {
+      path: '/moviedetail/:movieId',
+      name: 'moviedetail',
+      component: MovieDetail,
+      beforeEnter(to, from, next) {
+        // 如果是刷新来的就改成首页，否则就正常跳转
+        from.name === null ? next('/') : next();
+      }
+    },
+    {
+      path: '/music',
+      name: 'music',
+      component: Music,
+      beforeEnter(to, from, next) {
+        // 如果是刷新来的就改成首页，否则就正常跳转
+        from.name === null ? next('/') : next();
+      }
+    },
+    {
+      path: '/book',
+      name: 'book',
+      component: Book,
+      beforeEnter(to, from, next) {
+        from.name === null ? next('/') : next();
+      }
+    },
+    {
+      path: '/photo',
+      name: 'photo',
+      component: Photo,
+      beforeEnter(to, from, next) {
+        from.name === null ? next('/') : next();
+      }
+    },
+    {
+      path: '/photo/photodetail/:index',
+      name: 'photodetail',
+      component: PhotoDetail,
+      beforeEnter(to, from, next) {
+        from.name === null ? next('/') : next();
+      }
     }
-  },{
-    path: '/music',
-    name: 'music',
-    component: Music,
-    beforeEnter(to, from, next) {
-      // 如果是刷新来的就改成首页，否则就正常跳转
-      from.name === null ? next('/') : next();
-    }
-  }, {
-    path: '/book',
-    name: 'book',
-    component: Book,
-    beforeEnter(to, from, next) {
-      from.name === null ? next('/') : next();
-    }
-  }, {
-    path: '/photo',
-    name: 'photo',
-    component: Photo,
-    beforeEnter(to, from, next) {
-      from.name === null ? next('/') : next();
-    }
-  }, {
-    path: '/moviedetail/:movieId',
-    name: 'moviedetail',
-    component: MovieDetail
-  }],
+  ],
 });
