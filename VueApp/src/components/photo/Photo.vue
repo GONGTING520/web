c6d56f397c5e21ced6bb344be9643abfa904a7a7
GONGTING520 <954160311@qq.com>
2018-02-04 16:55:54 +0800

    给照片增加加载图片

--- VueApp/src/components/photo/Photo.vue
+++ VueApp/src/components/photo/Photo.vue
@@ -3,6 +3,7 @@
     <ul class="clearfix">
       <li class="img-li" @click="$router.push(`/photo/photodetail/${obj.id}`)" v-for="obj in photoList" :key="obj.id" :style="{background: 'url(' + obj.src + ') no-repeat center/cover'}"></li>
     </ul>
+    <div class="loading" v-show="isLoading"></div>
   </div>
 </template>
 
@@ -13,20 +14,24 @@ import store from "@/vuex/store.js";
 export default {
   data() {
     return {
-      photoList: []
+      photoList: [],
+      isLoading: true
     };
   },
   store,
   created() {
-    axios
-      .get("static/json/photo.json")
-      .then(res => {
-        this.photoList = Array.from(res.data);
-        this.$store.commit("setPhoto", Array.from(res.data));
-      })
-      .catch(() => {
-        console.log("error");
-      });
+    setTimeout(() => {
+      axios
+        .get("static/json/photo.json")
+        .then(res => {
+          this.photoList = Array.from(res.data);
+          this.$store.commit("setPhoto", Array.from(res.data));
+          this.isLoading = false;
+        })
+        .catch(() => {
+          console.log("error");
+        });
+    }, 1000);
   }
 };
 </script>
@@ -36,5 +41,14 @@ export default {
   width: 50%;
   padding-top: 50%;
   float: left;
+}
+.container .loading {
+  position: absolute;
+  top: 1rem;
+  bottom: 1rem;
+  left: 0;
+  right: 0;
+  background: rgba(255, 255, 255, 0.8) url(/static/img/loadingImg.gif) no-repeat
+    center/50%;
 }
 </style>
