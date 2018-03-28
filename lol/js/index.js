677a93dbc40e8eea364f33bd9156b5631c638ede
GONGTING520 <954160311@qq.com>
2018-03-28 22:25:21 +0800

    滚动时修改透明度

--- lol/js/index.js
+++ lol/js/index.js
@@ -1,4 +1,12 @@
 $(function () {
+    // 背景透明度改变部分
+    var iClentHeight = document.documentElement.clientHeight;
+    $(window).on('scroll', function(){
+        var iOpacity = 1 - $('body,html').prop('scrollTop')/iClentHeight;
+        $('.bg').css('opacity', iOpacity);
+    });
+
+    //  技能切换部分
     $(".skills .skill-select .skill-info").on('click', function () {
         $(this).addClass('selected').siblings().removeClass('selected');
         var index = $(this).index()
