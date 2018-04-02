78ddf99bd1cd0ae0ddbce90a159ccef1a0ae667a
GONGTING520 <954160311@qq.com>
2018-04-02 23:13:17 +0800

    新增精美背景切换部分和菜单显示部分

--- lol/js/index.js
+++ lol/js/index.js
@@ -14,4 +14,30 @@ $(function () {
             .addClass('selected').siblings()
             .removeClass('selected');
     });
+
+    //  精美背景切换部分
+    $(".backdrop-img .small-imgs li").on('click',function(){
+        $(this).addClass('selected').siblings().removeClass('selected');
+        $(".backdrop-img .imgs li").eq($(this).index()).addClass('selected').siblings().removeClass('selected');
+    });
+
+    //  菜单显示
+    $('.menu .open').on('click', function(){
+        $(this).removeClass('selected');
+        $('.menu .close').addClass('selected');
+        $('.layup').show();
+    });  
+    $('.menu .close').add('.layup').on('click', function(){
+        $('.menu .close').removeClass('selected');
+        $('.menu .open').addClass('selected');
+        $('.layup').hide();        
+    });
+    
+    // 菜单中的点击特效
+    $('.layup li').on('click', function(){
+        var iScrollTop = $($(this).attr('alt')).offset().top;
+        $('body,html').animate({
+            scrollTop: iScrollTop
+        }, 1000);
+    });
 });
